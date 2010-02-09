a923cc3ccea27339af2e735e574a72ba3d4b8c60
Sami Dalouche <sami.dalouche@gmail.com>
2010-02-08 21:00:14 -0500

    Created ShuffledDeck

---
+++ src/main/java/com/test/deck/ShuffledDeck.java
@@ -0,0 +1,20 @@
+package com.test.deck;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public final class ShuffledDeck implements Deck {
+
+	private List<PlayingCard> cards;
+	public ShuffledDeck(Deck deckToShuffle) {
+		super();
+		cards = new ArrayList<PlayingCard>(deckToShuffle.getCards());
+		Collections.shuffle(cards);
+	}
+
+	public List<PlayingCard> getCards() {
+		return cards;
+	}
+
+}
---
+++ src/test/java/com/test/deck/ShuffledDeckTest.java
@@ -0,0 +1,64 @@
+package com.test.deck;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ShuffledDeckTest {
+
+	@Test
+	public void shouldNotAlterCards() {
+		ShuffledDeck shuffledDeck = new ShuffledDeck(originalDeck());
+		Assert.assertThat(setOf(shuffledDeck.getCards()), is(setOf(sampleCards())));
+	}
+	
+	/**
+	 * FIXME: this test is theoretically not valid, as there is a _slight_ probability that the
+	 * shuffling produces exactly the same deck of playing cards. 
+	 * This test would be a good use of TestNG's "Success Rate" feature.
+	 * 
+	 * However, to keep things simple, let's keep things as-is for now
+	 * 
+	 */
+	@Test
+	public void orderOfCardsShouldBeDifferent() {
+		ShuffledDeck shuffledDeck = new ShuffledDeck(originalDeck());
+		assertThat(shuffledDeck.getCards(), is(not(sampleCards())));
+	}
+	
+	private Set<PlayingCard> setOf(List<PlayingCard> cards) {
+		return new HashSet<PlayingCard>(cards);
+	}
+	
+	private Deck originalDeck() {
+		Deck deck = mock(Deck.class);
+		Mockito.when(deck.getCards()).thenReturn(sampleCards()); 
+		return deck;
+	}
+	
+	private List<PlayingCard> sampleCards() {
+		return Arrays.asList(
+				(PlayingCard)new StandardCard(Rank.ACE, Suit.CLUBS),
+				new StandardCard(Rank.TEN, Suit.DIAMONDS),
+				new StandardCard(Rank.KING, Suit.SPADES),
+				new StandardCard(Rank.KING, Suit.DIAMONDS),
+				new StandardCard(Rank.QUEEN, Suit.SPADES),
+				new StandardCard(Rank.QUEEN, Suit.HEARTS),
+				new StandardCard(Rank.FOUR, Suit.CLUBS),
+				new StandardCard(Rank.FIVE, Suit.CLUBS),
+				new StandardCard(Rank.SIX, Suit.CLUBS),
+				new StandardCard(Rank.HEIGHT, Suit.CLUBS)
+		);
+	}
+}
