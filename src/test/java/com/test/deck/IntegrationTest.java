4bc38044eef19275ec0c22c57324d514405f57af
Sami Dalouche <sami.dalouche@gmail.com>
2010-02-08 22:35:32 -0500

    oops, forgot test

---
+++ src/test/java/com/test/deck/IntegrationTest.java
@@ -0,0 +1,28 @@
+package com.test.deck;
+
+import static com.test.deck.ShuffledCardProvider.shuffled;
+import static com.test.deck.UnshuffledCardProvider.cardsWithoutJokers;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Test
+	public void shouldGiveTwoCardsToEachPlayerWhileDealingOpeningHand() {
+		Player player1 = new SimplePlayer();
+		Player player2 = new SimplePlayer();
+		Deck deck = new DealableDeck(shuffled(cardsWithoutJokers()));
+		BlackjackDealer dealer = new BlackjackDealer(deck, Arrays.asList(player1, player2));
+		
+		dealer.dealOpeninghand();
+		
+		for(List<PlayingCard> cards : Arrays.asList(player1.getCards(), player2.getCards(), dealer.getCards())) {
+			assertEquals(2, cards.size());	
+		}
+		
+	}
+}
