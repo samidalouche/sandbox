a74654a2726e73be2503a7856807913e6e8192ea
Sami Dalouche <sami.dalouche@gmail.com>
2010-02-13 14:05:10 -0500

    better names in ShapeVisitor interface

--- shape-visitor/src/main/java/com/dalouche/experiments/shapevisitor/shapes/ShapeVisitor.java
+++ shape-visitor/src/main/java/com/dalouche/experiments/shapevisitor/shapes/ShapeVisitor.java
@@ -2,6 +2,6 @@ package com.dalouche.experiments.shapevisitor.shapes;
 
 public interface ShapeVisitor {
 	<T> T visitCircle(Circle circle);
-	<T> T visitTriangle(Triangle circle);
-	<T> T visitRectangle(Rectangle circle);
+	<T> T visitTriangle(Triangle triangle);
+	<T> T visitRectangle(Rectangle rectangle);
 }
