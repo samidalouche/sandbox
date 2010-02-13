54c055c181d15b3998571e31eca734a937ccfdd4
Sami Dalouche <sami.dalouche@gmail.com>
2010-02-13 13:42:39 -0500

    Used generics to avoid having TestUtils generate compilation errors

--- shape-visitor/src/test/java/com/dalouche/experiments/shapevisitor/commons/TestUtils.java
+++ shape-visitor/src/test/java/com/dalouche/experiments/shapevisitor/commons/TestUtils.java
@@ -6,24 +6,24 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class TestUtils {
-	public static void equalShouldBeReflexive(Object o) {
+	public static <T> void equalShouldBeReflexive(T o) {
 		assertThat(o, is(equalTo(o)));
 	}
 	
-	public static void shouldNotEqualNull(Object o) {
+	public static <T> void shouldNotEqualNull(T o) {
 		assertThat(o, is(not(equalTo(null))));
 	}
 	
-	public static void shouldNotEqualObjectOfDifferentType(Object o) {
+	public static <T> void shouldNotEqualObjectOfDifferentType(T o) {
 		assertThat(o, is(not(equalTo(anyObjectOfUnkownType()))));
 	}
 
-	public static void shouldBeEqualAndHaveSameHashCode(Object o1, Object o2) {
+	public static <T> void shouldBeEqualAndHaveSameHashCode(T o1, T o2) {
 		assertThat(o1, is(equalTo(o2)));
 		assertThat(o1.hashCode(), is(equalTo(o2.hashCode())));
 	}
 	
-	public static void shouldNotBeEqualAndHaveDifferentHashCode(Object o1, Object o2) {
+	public static <T> void shouldNotBeEqualAndHaveDifferentHashCode(T o1, T o2) {
 		assertThat(o1, is(not(equalTo(o2))));
 		assertThat(o1.hashCode(), is(not(equalTo(o2.hashCode()))));
 	}
